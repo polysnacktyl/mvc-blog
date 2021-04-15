10dae07de9277a428d6722afb4aa44e13971ada1
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-14 21:59:22 -0400

    /posts displaying all posts in db. /post/# displaying single post by id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Post = require('../models/Post');
 
 
-router.get('/', async (req, res) => {
+router.get('/posts', async (req, res) => {
   const postData = await Post.findAll().catch((err) => { 
       res.json(err);
     });
